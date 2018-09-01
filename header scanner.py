d151bac7dea607f74150347821b48b91299b57aa
MercuryJack <41839009+MercuryJack@users.noreply.github.com>
2018-09-01 03:29:34 -0400

    Title and Header Scraper
    Scrapes title and headings from a selected URL for SEO analysis.

---
+++ header scanner.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  1 02:07:13 2018
+
+@author: write
+"""
+
+# import libraries
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+#specify URL
+quote_page = input('What URL would you like to scrape?')
+
+#query website and return HTML to the variable page
+page = urlopen(quote_page)
+
+#parse the HTML with BeautifulSoup and store in variable 'soup'
+soup = BeautifulSoup(page, 'html.parser')
+
+#now we have the HTML as soup, so we need to grab the title and headers
+for x in soup:
+    title = soup.find('title')
+    h1s = soup.find('h1')
+    h2s = soup.find('h2')
+    h3s = soup.find('h3')
+    metadescription = soup.find('meta name="description"')
+
+
+#print out the data in readable format, including "none" for missing data types
+print()
+print('Title:')
+print(title)
+print()
+print('H1s:')
+print(h1s)
+print()
+print('H2s:')
+print(h2s)
+print()
+print('H3s:')
+print(h3s)
+print()
+print('Description:')
+print(metadescription)
